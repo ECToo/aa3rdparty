3fac96b9b1a659cd06f357dee771e6c1e783f767
davecarter85@gmail.com <davecarter85@gmail.com@fee489f3-10e0-652d-1be8-9a916ae2e075>
2012-02-26 02:17:39 +0000

    Added source code for AATime and GSAdminC AATime is stable. This GSAdminC version isn't stable, so please use the one included with the game installation or the one on the aa3rdparty googlecode page.

---
+++ trunk/tools/3PM/GSAdmin_Source/GSAdminC/programSettings.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GSAdminC
+{
+    public partial class programSettings : Form
+    {
+        Properties.Settings settings = new Properties.Settings();
+        Functions func = new Functions();
+        public programSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                #region checkbeforesave
+                if (chkStart.Checked)
+                {
+                    settings.startup = true;
+                }
+                else
+                {
+                    settings.startup = false;
+                }
+
+                if (chkAuto.Checked)
+                {
+                    settings.autorefresh = true;
+                    settings.interval = Convert.ToInt32(numMin.Value) * 60;
+
+                }
+                else
+                {
+                    settings.autorefresh = false;
+                    settings.interval = 0;
+                }
+
+                if (chkMinimize.Checked)
+                {
+                    if (settings.minimize == true)
+                    {
+                        settings.closeNotify = false;
+                    }
+                    else
+                    {
+                        settings.closeNotify = true;
+                    }
+                    settings.minimize = true;
+                    
+                }
+                else
+                {
+                    settings.minimize = false;
+                    settings.closeNotify = false;
+                }
+
+                
+                if (chkNotify.Checked)
+                {
+                    settings.notify = true;
+                }
+                else
+                {
+                    settings.notify = false;
+                }
+
+             /* Removed for now to stop confusion.
+              * if (chkBots.Checked)
+                {
+                    settings.showbots = true;
+                }
+                else
+                {
+                    settings.showbots = false;
+                }
+             */
+
+                if (chkJoin.Checked)
+                {
+                    if (txtLocation.Text == "")
+                    {
+                        throw new Exception("To enable join game, you must locate your installation of Alien Arena");
+                    }
+                    else
+                    {
+                        if (!func.validateText(txtPlayer.Text))
+                        {
+                            throw new Exception("To enable join game, you must insert a player name");
+                        }
+                       
+                        settings.playername = txtPlayer.Text;
+                        settings.enablejoin = true;
+                        settings.aalocation = txtLocation.Text;
+                        
+                    }
+                }
+                else
+                {
+                    settings.enablejoin = false;
+                    settings.aalocation = "";
+                    settings.playername = "";
+                }
+
+                settings.Save();
+                this.Close();
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                func.exceptionbox(ex.Message);
+            }
+
+        }
+
+        private void programSettings_Load(object sender, EventArgs e)
+        {
+
+            #region loadsettings
+            settings.Reload();
+
+            if(settings.startup){
+                chkStart.Checked = true;
+            }
+
+            if(settings.autorefresh){
+                chkAuto.Checked = true;
+                if (settings.interval == 0)
+                {
+                    numMin.Value = 1;
+                }
+                else
+                {
+                    numMin.Value = settings.interval / 60;
+                }
+            }
+
+            if(settings.minimize){
+                chkMinimize.Checked = true;
+            }
+
+            if(settings.notify){
+                chkNotify.Checked = true;
+            }
+
+            /* if (settings.showbots)
+            {
+                chkBots.Checked = true;
+            }
+            */
+            
+            if(settings.enablejoin){
+                if(settings.aalocation != "" && settings.playername != ""){
+                    chkJoin.Checked = true;
+                    txtLocation.Text = settings.aalocation;
+                    txtPlayer.Enabled = true;
+                    txtPlayer.Text = settings.playername;
+                }else{
+                    chkJoin.Checked= false;
+                    txtPlayer.Enabled = false;
+                }
+            }
+
+            if (chkJoin.Checked)
+            {
+                btnBrowse.Enabled = true;
+            }
+            else
+            {
+                btnBrowse.Enabled = false;
+            }
+            
+
+            #endregion
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult returned = new DialogResult();
+            returned = gameBrowse.ShowDialog();
+            //string location = gameBrowse.SelectedPath();
+            try
+            {
+                if (returned == DialogResult.OK)
+                {
+                    if (!File.Exists(gameBrowse.SelectedPath + "\\crx.exe"))
+                    {
+                        txtLocation.Text = "";
+                        txtPlayer.Enabled = false;
+                        txtPlayer.Text = "";
+                        throw new Exception("Alien Arena not found in " + gameBrowse.SelectedPath);
+                    }
+                    else
+                    {
+                        func.successbox("Alien Arena located");
+                        txtPlayer.Enabled = true;
+                        txtLocation.Text = Convert.ToString(gameBrowse.SelectedPath);
+                    }
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                func.exceptionbox(ex.Message);
+            }
+            
+
+        }
+
+        private void chkJoin_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkJoin.Checked)
+            {
+                btnBrowse.Enabled = true;
+            }
+            else
+            {
+                btnBrowse.Enabled = false;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult response = new DialogResult();
+
+            response = MessageBox.Show("This will delete all settings, and servers. Are you sure you want to continue?","Question.",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+
+            if (response == DialogResult.Yes)
+            {
+                settings.Reset();
+                settings.Save();
+                settings.Reload();
+                this.Dispose();
+            }
+        }
+    }
+}
